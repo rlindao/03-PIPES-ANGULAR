2124e6168fbe0ad134777a859d5a02d546a7c7a4
rlindao <lindaorobertoandres@gmail.com>
2021-03-21 20:18:27 -0500

    limpieza de archivos

--- src/app/pipes/contraseña.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ContraseñaPipe } from './contraseña.pipe';
-
-describe('ContraseñaPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ContraseñaPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
